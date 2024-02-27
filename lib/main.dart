44cd8f8d6ce8312fb2c9b4457d163ad0a04bd967
vepraaa <a.veprikovaa@yandex.ru>
2024-02-27 17:54:40 +0500

    молюсь

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,229 @@
 import 'package:flutter/material.dart';
+import 'my_styles.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const XOGame());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class XOGame extends StatelessWidget {
+  const XOGame({super.key});
 
   // This widget is the root of your application.
   @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const XOGamePage(title: 'XO GAME'),
+      
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class XOGamePage extends StatefulWidget {
+  const XOGamePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<XOGamePage> createState() => _XOGamePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _XOGamePageState extends State<XOGamePage> {
+  bool isTurn = true;
+  int oScore = 0;
+  int xScore = 0;
+  int count = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<String> changeXO = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('XO Game'),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 86, 129, 250),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Color.fromARGB(243, 225, 225, 254),
+        body: Column(
+          children: [
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Text(
+                          'Player Х',
+                          style: txtStyle,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          xScore.toString(),
+                          style: txtStyle,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Text(
+                          'Player О',
+                          style: txtStyle,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          oScore.toString(),
+                          style: txtStyle,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )),
+            Expanded(
+                flex: 3,
+                child: GridView.builder(
+                    itemCount: changeXO.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3),
+                    itemBuilder: (BuildContext ctx, int index) {
+                      return GestureDetector(
+                        onTap: () => _setXorO(index),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blueGrey)),
+                          child: Center(
+                              child: Text(
+                            changeXO[index],
+                            style: xoStyle,
+                          )),
+                        ),
+                      );
+                    })),
+          ],
+        ));
+  }
+
+  void _setXorO(int i) {
+    if (isTurn && changeXO[i] == '') {
+      setState(() {
+        changeXO[i] = 'o';
+        isTurn = !isTurn;
+      });
+    } else if (!isTurn && changeXO[i] == '') {
+      setState(() {
+        changeXO[i] = 'x';
+        isTurn = !isTurn;
+      });
+    }
+
+    count++;
+    _checkWinner();
+  }
+
+  void _checkWinner() {
+    if (changeXO[0] == changeXO[1] &&
+        changeXO[0] == changeXO[2] &&
+        changeXO[0] != '') {
+      _showDialog(winner: changeXO[0]);
+    }
+    if (changeXO[3] == changeXO[4] &&
+        changeXO[3] == changeXO[5] &&
+        changeXO[3] != '') {
+      _showDialog(winner: changeXO[3]);
+    }
+    if (changeXO[6] == changeXO[7] &&
+        changeXO[6] == changeXO[8] &&
+        changeXO[6] != '') {
+      _showDialog(winner: changeXO[6]);
+    }
+    if (changeXO[0] == changeXO[3] &&
+        changeXO[0] == changeXO[6] &&
+        changeXO[0] != '') {
+      _showDialog(winner: changeXO[0]);
+    }
+    if (changeXO[1] == changeXO[4] &&
+        changeXO[1] == changeXO[7] &&
+        changeXO[1] != '') {
+      _showDialog(winner: changeXO[1]);
+    }
+    if (changeXO[2] == changeXO[5] &&
+        changeXO[2] == changeXO[8] &&
+        changeXO[2] != '') {
+      _showDialog(winner: changeXO[2]);
+    }
+    if (changeXO[0] == changeXO[4] &&
+        changeXO[0] == changeXO[8] &&
+        changeXO[0] != '') {
+      _showDialog(winner: changeXO[0]);
+    }
+    if (changeXO[2] == changeXO[4] &&
+        changeXO[2] == changeXO[6] &&
+        changeXO[2] != '') {
+      _showDialog(winner: changeXO[2]);
+    }
+
+    if (count == 9) {
+      _showDialog(winnerExist: false);
+      _clearBoard();
+    }
+  }
+
+  void _showDialog({String winner = '', bool winnerExist = true}) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(winnerExist ? 'Победитель: $winner' : 'Нет победителя'),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Играть ещё раз!'))
+            ],
+          );
+        });
+    count = 0;
+    _clearBoard();
+
+    if (winner == 'o') {
+      setState(() {
+        oScore++;
+      });
+    } else if (winner == 'x') {
+      setState(() {
+        xScore++;
+      });
+    }
+  }
+
+  void _clearBoard() {
+    for (int i = 0; i < 9; i++) {
+      changeXO[i] = '';
+    }
   }
 }
---
+++ lib/my_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle xoStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0, color: Color.fromARGB(255, 86, 129, 250));
+
+const TextStyle txtStyle = TextStyle(
+    fontWeight: FontWeight.bold, fontSize: 30.0, color: Color.fromARGB(255, 86, 129, 250));
